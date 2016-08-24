08748724d366a5908b993301113a0e570e12ce74
ShazXVII <erikskr99@gmail.com>
2016-08-24 19:00:23 +0200

    changes

--- push2git.py
+++ push2git.py
@@ -1,4 +1,5 @@
 import os
+import time
 location = "cd shazxvii.github.io"
 add = "git add *"
 note = input("please enter what you committed: ")
@@ -7,7 +8,12 @@ push = "git push -u origin master"
 usr = "ShazXVII"
 pwd = "Bananasplit99"
 os.system(note)
+time.sleep(5)
 os.system(location)
+time.sleep(5)
 os.system(add)
+time.sleep(5)
 os.system(commit)
+time.sleep(5)
 os.system(push)
+time.sleep(15)
