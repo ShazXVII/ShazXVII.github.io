0b12e210de60b270cc4bb7115fb357985c73a097
ShazXVII <erikskr99@gmail.com>
2016-08-24 19:13:24 +0200

    automated push

--- push2git.py
+++ push2git.py
@@ -2,18 +2,11 @@ import os
 import time
 location = "cd shazxvii.github.io"
 add = "git add *"
-note = ("automated push")
-commit = "git commit -m" + "'" + note + "'"
+commit = "git commit -m 'automated push'"
 push = "git push -u origin master"
-usr = "ShazXVII"
-pwd = "Bananasplit99"
+
 
 os.system(add)
-time.sleep(5)
 os.system(commit)
-time.sleep(5)
+time.sleep(1)
 os.system(push)
-time.sleep(10)
-os.system("echo usr")
-time.sleep(2)
-os.system("echo pwd")
