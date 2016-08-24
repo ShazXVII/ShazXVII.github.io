a8a326b39dfc96e7f5b921cd844386b55fdfa04c
ShazXVII <erikskr99@gmail.com>
2016-08-24 18:57:56 +0200

    changes

---
+++ push2git.py
@@ -0,0 +1,13 @@
+import os
+location = "cd shazxvii.github.io"
+add = "git add *"
+note = input("please enter what you committed: ")
+commit = "git commit -m" + "'" + note + "'"
+push = "git push -u origin master"
+usr = "ShazXVII"
+pwd = "Bananasplit99"
+os.system(note)
+os.system(location)
+os.system(add)
+os.system(commit)
+os.system(push)
